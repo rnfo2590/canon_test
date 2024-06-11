0582be76048b922684caaf2ded557898b4dd1204
student <student@WIN-NCP6AQJ13NT>
2024-06-11 14:21:20 +0900

    Kaho1st

---
+++ src/p01/Kaho.java
@@ -0,0 +1,35 @@
+package p01;
+
+public class Kaho {
+	private Integer age;
+	private String name;
+	
+	public Kaho() {
+		
+	}
+	
+	public Kaho(Integer age, String name) {
+		super();
+		this.age = age;
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
--- src/p01/Sample01.java
+++ src/p01/Sample01.java
@@ -3,8 +3,11 @@ package p01;
 public class Sample01 {
 
 	public static void main(String[] args) {
+
 		System.out.println("深沢梨乃");
 		System.out.println("22");
+		
+		
 
 	}
 
